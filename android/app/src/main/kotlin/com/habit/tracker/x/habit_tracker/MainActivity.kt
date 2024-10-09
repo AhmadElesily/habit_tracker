dbd819fa8ac269f10a31f64489a71d2908aa73d8
ahmed.waleed200545@gmail.com <ahmed.waleed200545@gmail.com>
2024-10-09 09:00:53 +0200

    First Commit of The Project # it has design appbar ui # fonts 1 bold and 1 light

---
+++ android/app/src/main/kotlin/com/habit/tracker/x/habit_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.habit.tracker.x.habit_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: homePage(),
+  ));
+}
+
+class homePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(194),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Color(0xff6959D6),
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(40),
+                )),
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              title: Container(
+                width: double.infinity,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    SizedBox(height: 10,),
+                    Text(
+                      '! مساء الخير احمد',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Montserrat',
+                          fontSize: 24
+                      ),
+                    ),
+                    Text(
+                      'تتبع عاداتك الصباحية',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'MontserratLight',
+                          fontSize: 16
+                      ),
+                      softWrap: true,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )),
+      backgroundColor: Color(0xff151515),
+    );
+  }
+}
