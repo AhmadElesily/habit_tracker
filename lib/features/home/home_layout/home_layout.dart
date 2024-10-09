28841ec89c9b14d607e12224dba2c4f4715070e6
Ahmed Nafe <abonafe27@gmail.com>
2024-10-09 18:33:09 +0300

    Finish BottomAppBar And FloatingActionButton

---
+++ lib/features/home/home_layout/home_layout.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/colors.dart';
+
+class HomeLayout extends StatelessWidget {
+  const HomeLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.backgroundColor,
+    );
+  }
+}
