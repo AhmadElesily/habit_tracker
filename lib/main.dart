6d9d6a41324b722e37147d2c57f74521d627a280
Ahmed Nafe <abonafe27@gmail.com>
2024-10-12 19:35:49 +0300

    Add SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:habit_tracker/features/home/home.dart';
+import 'package:habit_tracker/features/home/home_page.dart';
+import 'package:habit_tracker/splash_screen/splash_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      initialRoute: SplashScreen.routeName,
+      routes:  {
+        SplashScreen.routeName: (context) => SplashScreen(),
+        HomePage.routeName: (context) => HomePage(),
+      },
     ),
   );
 }
