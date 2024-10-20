5443b3a51b779a929daf55e96fee5d19f5087c10
ahmed.waleed200545@gmail.com <ahmed.waleed200545@gmail.com>
2024-10-20 17:21:14 +0200

    adding Hive database logic to the project

--- lib/features/cubit/items_cubit_cubit.dart
+++ lib/features/cubit/items_cubit_cubit.dart
@@ -1,16 +1,37 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:habit_tracker/model/items_model.dart';
-
+import 'package:hive/hive.dart';
 import 'items_cubit_state.dart';
-
 
 
 class ItemsCubit extends Cubit<ItemsState> {
   ItemsCubit() : super(ItemsInitial());
-
+  final habitsBox = Hive.box('habits');
   List<ItemModel> items = [];
 
+
+  void fetchTodos() {
+    for (final item in habitsBox.toMap().values) {
+      final habit = ItemModel(
+        text: item['text'],
+        selectedDays: item['selectedDays'],
+        color: item['color'],
+        isSelected: item['isSelected'],
+        iconImage: item['iconImage'],
+      );
+      items.add(habit);
+    }
+  }
+
   void addItemsToList(ItemModel itemModel) {
+    habitsBox.add({
+      'text': itemModel.text,
+      'iconImage': itemModel.iconImage,
+      'isSelected': itemModel.isSelected,
+      'selectedDays': itemModel.selectedDays,
+      'color': itemModel.color,
+    });
+
     items.add(itemModel);
     emit(ItemsSucceed(items));
   }
@@ -19,6 +40,7 @@ class ItemsCubit extends Cubit<ItemsState> {
     items.removeAt(index);
     emit(ItemsSucceed(items));
   }
+
   void updateItemsInList(int index, ItemModel updatedItem) {
     items[index] = updatedItem;
     emit(ItemsSucceed(items));
@@ -26,9 +48,10 @@ class ItemsCubit extends Cubit<ItemsState> {
 
   int getCurrentDayIndex(selectedDay) {
     int today = selectedDay.weekday;
-    print("Today Number____${today-1}");
-    return today-1 ;
+    print("Today Number____${today - 1}");
+    return today - 1;
   }
+
   //== 7 ? 0 : today
   void loadHabitsForToday(selectedDay) {
     int currentDay = getCurrentDayIndex(selectedDay);
